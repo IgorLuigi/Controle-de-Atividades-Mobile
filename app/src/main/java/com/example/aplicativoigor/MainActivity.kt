2522700e553b7aaf3bc89120cd00e84b8b0a9f2f
IgorLuigi <igor.luigi@hotmail.com>
2021-11-29 20:17:20 -0300

    Concluindo ReclyclerView

--- app/src/main/java/com/example/aplicativoigor/MainActivity.kt
+++ app/src/main/java/com/example/aplicativoigor/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.aplicativoigor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.aplicativoigor.adapter.AtividadesAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iniciarRecyclerView()
+    }
+
+    private fun iniciarRecyclerView() {
+        recyclerViewAtividades.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewAtividades.adapter = AtividadesAdapter(Datasource.getAtividades())
     }
 }
