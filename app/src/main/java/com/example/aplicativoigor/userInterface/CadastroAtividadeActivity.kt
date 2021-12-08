58a4ea8d9512a629c2b56d25ba809033880623aa
IgorLuigi <igor.luigi@hotmail.com>
2021-12-07 22:31:34 -0300

    Personalizando a Toolbar

--- app/src/main/java/com/example/aplicativoigor/userInterface/CadastroAtividadeActivity.kt
+++ app/src/main/java/com/example/aplicativoigor/userInterface/CadastroAtividadeActivity.kt
@@ -2,6 +2,9 @@ package com.example.aplicativoigor.userInterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.aplicativoigor.R
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -17,5 +20,28 @@ class CadastroAtividadeActivity : AppCompatActivity() {
     private fun insertToolbar(){
         setSupportActionBar(toolbar)
         supportActionBar!!.title = "Nova Atividade"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_atividade, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.menu_cancelar -> {
+                Toast.makeText(this, "Cancelar", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_salvar -> {
+                Toast.makeText(this, "SALVAR", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                onBackPressed()
+            }
+        }
+
+        return true
     }
 }
