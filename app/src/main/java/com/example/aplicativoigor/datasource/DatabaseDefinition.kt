140134f333444df9bba2b67b3c57841f5bb68b6d
IgorLuigi <igor.luigi@hotmail.com>
2021-12-18 16:00:13 -0300

    Criando um DatabaseHelper

---
+++ app/src/main/java/com/example/aplicativoigor/datasource/DatabaseDefinition.kt
@@ -0,0 +1,18 @@
+package com.example.aplicativoigor.datasource
+
+class DatabaseDefinition {
+
+    object Atividade {
+        const val TABLE_NAME = "Atividade"
+
+        object Columns {
+            const val ID = "id"
+            const val DESCRICAO = "descricao"
+            const val DATA = "data"
+            const val PRIORIDADE = "prioridade"
+            const val TIPO_ATIVIDADE = "tipo_atividade"
+            const val ZERADO = "zerado"
+        }
+    }
+
+}
--- app/src/main/java/com/example/aplicativoigor/datasource/DatabaseHelper.kt
+++ app/src/main/java/com/example/aplicativoigor/datasource/DatabaseHelper.kt
@@ -5,8 +5,8 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(context: Context) : SQLiteOpenHelper (context, DATABASE_NAME, null, DATABASE_VERSION) {
-    override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(CREATE_TABLE_ATIVIDADE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -16,5 +16,14 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper (context, DATABASE_NAM
     companion object {
         private const val DATABASE_NAME = "atividadebanco"
         private const val DATABASE_VERSION = 1
+
+        private const val CREATE_TABLE_ATIVIDADE = "CREATE TABLE ${DatabaseDefinition.Atividade.TABLE_NAME} (" +
+                "${DatabaseDefinition.Atividade.Columns.ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "${DatabaseDefinition.Atividade.Columns.DESCRICAO} TEXT, " +
+                "${DatabaseDefinition.Atividade.Columns.DATA} TEXT, " +
+                "${DatabaseDefinition.Atividade.Columns.PRIORIDADE} REAL, " +
+                "${DatabaseDefinition.Atividade.Columns.TIPO_ATIVIDADE} TEXT, " +
+                "${DatabaseDefinition.Atividade.Columns.ZERADO} INTEGER);"
+
     }
 }
