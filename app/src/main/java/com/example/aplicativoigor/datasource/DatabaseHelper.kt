15de73b4491d13b5afccb08760a3a4882bf6f324
IgorLuigi <igor.luigi@hotmail.com>
2021-12-18 15:45:51 -0300

    Configurando o DatabaseHelper

--- app/src/main/java/com/example/aplicativoigor/datasource/DatabaseHelper.kt
+++ app/src/main/java/com/example/aplicativoigor/datasource/DatabaseHelper.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DatabaseHelper(context: Context) : SQLiteOpenHelper (context, "", null, 1) {
+class DatabaseHelper(context: Context) : SQLiteOpenHelper (context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
         TODO("Not yet implemented")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val DATABASE_NAME = "atividadebanco"
+        private const val DATABASE_VERSION = 1
     }
 }
