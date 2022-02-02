e499122288d6963ac8f9df82d2f0d20b7a98dd55
IgorLuigi <igor.luigi@hotmail.com>
2022-02-01 21:36:38 -0300

    Implementando o funcionamento do DELETE

--- app/src/main/java/com/example/aplicativoigor/adapter/AtividadesAdapter.kt
+++ app/src/main/java/com/example/aplicativoigor/adapter/AtividadesAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.aplicativoigor.adapter
+import android.app.AlertDialog
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.aplicativoigor.model.Atividade
 import androidx.recyclerview.widget.RecyclerView
 import com.example.aplicativoigor.R
 import com.example.aplicativoigor.constants.Constants
+import com.example.aplicativoigor.repository.AtividadeRepository
 import com.example.aplicativoigor.userInterface.CadastroAtividadeActivity
 import kotlinx.android.synthetic.main.layout_lista_atividades.view.*
 
@@ -39,6 +42,24 @@ class AtividadesAdapter(var listaAtividades: ArrayList<Atividade>) : RecyclerVie
                 intent.putExtra("opercao", Constants.OPERACAO_CONSULTAR)
                 intent.putExtra("id", atividade.id)
                 itemView.context.startActivity(intent)
+            }
+
+            itemView.setOnLongClickListener{
+
+                AlertDialog.Builder(itemView.context)
+                    .setTitle("Exclusão")
+                    .setMessage("Deseja excluir a atividade ${atividade.descricao}?")
+                    .setPositiveButton("SIM"){dialog, which ->
+                        val repo = AtividadeRepository(itemView.context)
+                        repo.delete(atividade.id)
+                        Tost.makeText(itemView.context, "Atividade escluída com sucesso!", Toast.LENGTH_SHORT).show()
+                    }
+                    .setNegativeButton("NÃO"){dialog, which ->
+
+                    }
+                    .show()
+
+                true
             }
         }
     }
